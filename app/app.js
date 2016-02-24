c9937a5590396590db6d2bb0d800279cc69f5117
kenneth <post.krj@icloud.com>
2016-02-24 13:15:09 +0100

    fixed scrolling issue.

--- app/app.js
+++ app/app.js
@@ -2,61 +2,70 @@ require(["./config"], function(){
 
     //main app
     require(["jquery", "view", "qr", "cam"], function($, view, qr, cam){
-/*
-
-        var canvas = $("#cam").get(0),
-            ctx = canvas.getContext('2d'),
-            scanFrame = true; //look for qr-code in frames
-
-        qr.init();
-        qr.on("decoded", function(event, data){
-            var regex = new RegExp("^[0-9]{3}-[0-9]{4}$", "i");
-            if ((data.length > 0) && !regex.test(data)) {
-                scanFrame = true; //not valid qr code, keep scanning frames
-            }else{
-                cam.stop();
-                $("#cam").addClass("hidden");
-                $("#location_id").val(data);
-            }
-
-        });
-        qr.on("error", function(event, error){
-            console.log("error:", error);
-            scanFrame = true;
-        });
-
-        cam.init(function(){
-            console.log("init cam");
-            $("#accessCam").on("click", function(){
-                $("#cam").removeClass("hidden");
-                cam.start();
-                scanFrame = true;
-            });
-            cam.on("capture", function(event, frame){
-                ctx.putImageData(frame, 0, 0);
-                if(scanFrame){
-                    var img = canvas.toDataURL('image/png');
-                    if(img){
-                        qr.scan(img);
-                    }
-
-                    scanFrame = false; //don't scan more than one frame at the same time (reduce cpu time)
-                }
-            });
-        });
-*/
+        var callStack = [];
 
         view.init($("#view"));
+        view.on("change", function(event, page){
+
+            if(page == "home") {
+                var canvas = $("#cam").get(0),
+                    ctx = canvas.getContext('2d'),
+                    scanFrame = true; //look for qr-code in frames
+
+                qr.init();
+                qr.on("decoded", function (event, data) {
+                    var regex = new RegExp("^[0-9]{3}-[0-9]{4}$", "i");
+                    if ((data.length > 0) && !regex.test(data)) {
+                        scanFrame = true; //not valid qr code, keep scanning frames
+                    } else {
+                        cam.stop();
+                        $("#cam").addClass("hidden");
+                        $("#location_id").val(data);
+                    }
+
+                });
+                qr.on("error", function (event, error) {
+                    console.log("error:", error);
+                    scanFrame = true;
+                });
+
+                cam.init(function () {
+                    console.log("init cam");
+                    $(document).on("click", "#accessCam", function () {
+                        if($("#cam:not(.hidden)").length){ //cam is running, stop it
+                            $("#cam").addClass("hidden");
+                            cam.stop();
+                            scanFrame = true;
+                        }else{
+                            $("#cam").removeClass("hidden");
+                            cam.start();
+                            scanFrame = true;
+                        }
+                    });
+                    cam.on("capture", function (event, frame) {
+                        ctx.putImageData(frame, 0, 0);
+                        if (scanFrame) {
+                            var img = canvas.toDataURL('image/png');
+                            if (img) {
+                                qr.scan(img);
+                            }
+
+                            scanFrame = false; //don't scan more than one frame at the same time (reduce cpu time)
+                        }
+                    });
+                });
+            }
+        });
         view.home();
 
-        $("nav").on("click", "li", function(){
+        $(document).on("click", "nav li", function(){
            var target = $(this).find("a").data("target");
             try {
                 view[target]();
                 $("nav ul li").removeClass("active");
                 $(this).addClass("active");
             }catch(e){
-                console.log("View: " + target +", not found");
+                console.log("View: " + target + ", not found");
             }
         });
 
--- app/lib/classes/view.js
+++ app/lib/classes/view.js
@@ -23,34 +23,49 @@ define(["jquery", "twig"], function($, twig) {
         async: false
     });
 
+    var cb = function(page){
+        $(container).trigger("change", [page]).data("active", page);
+    };
+
     return {
-        init: function(containerElement){
+        init: function(containerElement, callback){
             container = containerElement;
+            if(callback){
+                cb = callback;
+            }
+        },
+        on: function(event, callback){
+            $(container).on(event, callback);
         },
         login: function(){
             $(container).html(templates.login.render({
                 key: "world"
             }));
+            cb("login");
         },
         home: function(){
             $(container).html(templates.home.render({
                 key: "world"
             }));
+            cb("home");
         },
         map: function(){
             $(container).html(templates.map.render({
                 key: "world"
             }));
+            cb("map");
         },
         stats: function(){
             $(container).html(templates.stats.render({
                 key: "world"
             }));
+            cb("stats");
         },
         settings: function(){
             $(container).html(templates.settings.render({
                 key: "world"
             }));
+            cb("settings");
         }
     }
 });
